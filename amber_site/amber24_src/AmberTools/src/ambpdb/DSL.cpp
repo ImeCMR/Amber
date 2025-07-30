03a8228cb7e99ff2e3540f304ddac32a5af70dde
Imesh Ranaweera <imeshchamara57@gmail.com>
2025-07-30 17:51:28 +0000

    Seperated the src

--- amber_site/amber24_src/AmberTools/src/ambpdb/DSL.cpp
+++
@@ -1,9 +0,0 @@
-#include "DSL.h"
-
-DataSet::DataSet() {}
-
-DataSetList::DataSetList() {}
-
-DataSetList::~DataSetList() {}
-
-DataSet* DataSetList::GetDataSet(std::string const&) const { return 0; }
